5e1520199ef253bd4b2b4ca4dd3030e813a24d03
pvpatil5 <pvpatil123@yahoo.com>
2020-09-11 09:32:38 +0530

    Revert "Third commit class1 Modified and Class3 Added"
    This reverts commit 4d8432362922d144dadddda2b014453b1403384c.

--- src/pac1/Class1.java
+++ src/pac1/Class1.java
@@ -5,7 +5,16 @@ public class Class1 {
 	public static void main(String[] args) 
 
 	{
-		System.out.println("Class1");
+		String s1= "Selenium";
+		String s2 = "";
+
+		char c[] = s1.toCharArray();
+
+		for (int i = c.length-1; i >=0; i--) 
+		{
+			s2= s2+c[i];
+		}
+		System.out.println(s2);
 	}
 
 }
