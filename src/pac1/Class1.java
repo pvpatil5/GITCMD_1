96539ec6e6a3d5013f50a7e5af8da85cb2bdc834
pvpatil5 <pvpatil123@yahoo.com>
2020-09-11 09:16:01 +0530

    first commit class1

---
+++ src/pac1/Class1.java
@@ -0,0 +1,20 @@
+package pac1;
+
+public class Class1 {
+
+	public static void main(String[] args) 
+
+	{
+		String s1= "Selenium";
+		String s2 = "";
+
+		char c[] = s1.toCharArray();
+
+		for (int i = c.length-1; i >=0; i--) 
+		{
+			s2= s2+c[i];
+		}
+		System.out.println(s2);
+	}
+
+}
